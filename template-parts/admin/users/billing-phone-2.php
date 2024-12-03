6a0cfde02ec4b09ac28d000c68d8493d81037043
Jonathan ALCARAS <lecyclopeduweb@gmail.com>
2024-12-03 18:17:02 +0100

    Fixed bug Display billing_phone_2 on BO user account

--- template-parts/admin/users/billing-phone-2.php
+++ template-parts/admin/users/billing-phone-2.php
@@ -11,5 +11,8 @@
 <table id="fieldset-billing-phone-2" class="form-table">
     <tr>
         <th><label for="billing_phone_2"><?= __('Phone 2',UTBF_TEXT_DOMAIN); ?></label></th>
+        <td>
+            <input type="text" name="billing_phone_2" id="billing_phone_2" value="<?= get_user_meta( $args['user']->ID,'billing_phone_2', true); ?>" class="regular-text" /><br />
+        </td>
     </tr>
 </table>
